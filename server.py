a1a82cae454de92aa7e95cf8e27a997258d0ba62
Eric Harmeling <eric.harmeling@sas.com>
2018-06-12 16:27:50 -0400

    CORS added, image added to mine endpoint

--- server.py
+++ server.py
@@ -1,17 +1,29 @@
 from blocks import Chain
 from flask import Flask, request
+from flask_cors import CORS
 import datetime
 import requests
 import json
 
 app = Flask(__name__)
+CORS(app)
+
+node_id =
+
+global_target = "Car"
 
 blockchain = Chain()
+
+temp_store = 'path' # temporary image store location
 
 peers = set()
 
 @app.route('/transaction', methods=['POST'])
 def new_transaction():
+    """
+    Posts new transaction to chain.
+    :return:
+    """
     data = request.get_json()
     fields = ["sender", "recipient", "quantity"]
 
@@ -25,23 +37,30 @@ def new_transaction():
 
     return "OK", 201
 
+
 @app.route('/chain', methods=['GET'])
 def get_chain():
+    """
+    Get request to return chain.
+    :return: Returns chain length and block data.
+    """
     data = []
     for block in blockchain.chain:
         data.append(block.__dict__)
     return json.dumps({"length": len(data), "chain": data})
 
-@app.route('/mine', methods=['GET'])
+@app.route('/mine', methods=['POST'])
 def mine():
+    image_file = request.file['file']
+    image_file.save(temp_store)
+
     last_block = blockchain.last_block
-    proof = blockchain.proof_of_work(last_block)
+    proof = blockchain.proof_of_work(last_block, image_file, global_target)
 
     blockchain.add_transaction_fields(sender="God", recipient=node_id, quantity=1)
 
-    block = blockchain.add_block(data, proof)
-
-
+    block = blockchain.new_block(image_file, blockchain.transactions, proof)
+    blockchain.add_block(block)
 
 def consensus():
 
