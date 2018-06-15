ca6633299874918349733a6ec57cc1cd4e04c378
ericharmeling <ejharmel@ncsu.edu>
2018-06-14 21:50:22 -0400

    target changed to label

--- images.py
+++ images.py
@@ -1,10 +1,10 @@
 import subprocess
 
-def image_match(file, target):
+def image_match(file, label):
     """
     :param file: Input image file
     :param target: Target image category
     :return: True if image from file matches image category, False if not.
     """
     image = subprocess.run(["models/tutorials/image/imagenet/classify_image.py", "--image-file" + file])
-    return image == target
+    return image == label
