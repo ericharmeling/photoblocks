465f126e5a9968b3ed2e51e942d824355086e543
Eric Harmeling <eric.harmeling@sas.com>
2018-06-27 17:10:12 -0400

    Added node register mechanism

--- interface.py
+++ interface.py
@@ -4,17 +4,24 @@ from uuid import uuid4
 import requests
 import json
 
+# HEAD NODE INITIALIZATION VARIABLES
+
 app = Flask(__name__)
 
-node_id = str(uuid4()).replace('-', '')  # unique node ID
-node_key = 0  # node public key (for mining rewards)
+head_node_name = "Head Node"
+head_node_id = str(uuid4()).replace('-', '')  # head node's unique node ID for identification
+head_node_type = "HEAD"
+head_node_key = 0
 
-blockchain = Chain()
+blockchain = Chain()  # initialize blockchain
+nodes = list()  # initialize nodes
+nodes.append({'node_name': head_node_name, 'node_id': head_node_id, 'node_type': head_node_type,
+              'node_key': head_node_key})
 
-temp_store = 'path'  # temporary image store location
+temp_store = '/temp/'  # temporary image store location
 
-nodes = set()
 
+# WEB PAGES
 
 @app.route('/')
 def index():
@@ -31,6 +38,12 @@ def mine_page():
     return render_template('mine.html')
 
 
+@app.route('/nodes')
+def nodes_page():
+    global nodes
+    return render_template('nodes.html', peers=nodes)
+
+
 @app.route('/about')
 def about():
     return render_template('about.html')
@@ -41,9 +54,28 @@ def contact():
     return render_template('contact.html')
 
 
+# WEB REQUESTS
+
+# Nodes
+
 @app.route('/nodes/register', methods=['POST'])
 def register():
-    pass
+    if request.method == 'POST':
+        node_name = request.form['node_name']
+        node_id = request.form['node_id']
+        node_type = request.form['node_type']
+        node_key = request.form['node_key']
+        nodes.append({node_name, node_id, node_type, node_key})
+
+    return nodes
+
+
+@app.route('/nodes/list', methods=['GET'])
+def list_nodes():
+    return nodes
+
+
+# Trading
 
 
 @app.route('/transaction', methods=['POST'])
@@ -67,23 +99,15 @@ def transaction():
     return "OK", 201
 
 
-@app.route('/chain', methods=['GET'])
-def chain():
-    """
-    Get request to return chain.
-    :return: Returns chain length and block data.
-    """
-    data = []
-    for block in blockchain.chain:
-        data.append(block.__dict__)
-    return json.dumps({"length": len(data), "chain": data})
+# Mining
 
 
 @app.route('/mine', methods=['POST', 'GET'])
 def mine():
     if request.method == 'POST':
-        image_file = request.file['file']
+        image_file = request.form['file']
         label = request.form['label']
+        last_label = request.form['last_label']
     else:
         image_file = []
         label = "Nothing"
@@ -96,6 +120,20 @@ def mine():
 
     blockchain.transactions = []
     blockchain.add_transaction_fields(sender="God", recipient=node_key, quantity=1)
+
+    return blockchain
+
+
+@app.route('/chain', methods=['GET'])
+def chain():
+    """
+    Get request to return chain.
+    :return: Returns chain length and block data.
+    """
+    data = []
+    for block in blockchain.chain:
+        data.append(block.__dict__)
+    return json.dumps({"length": len(data), "chain": data})
 
 
 def consensus():
