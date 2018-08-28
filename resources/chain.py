422c7ad1ada6bdd683d106a15a8e0d1a3386ef31
ericharmeling <ejharmel@ncsu.edu>
2018-08-27 20:28:43 -0700

    Resources cleaned up

--- resources/chain.py
+++ resources/chain.py
@@ -1,13 +1,12 @@
 from flask.views import MethodView
-from flask import request
 import json
 
 
 class ChainResource(MethodView):
     def get(self):
         data = []
-        
+
         for block in blockchain.chain:
             data.append(block.__dict__)
-        
+
         return json.dumps({"length": len(data), "chain": data}), 200
--- resources/nodes.py
+++ resources/nodes.py
@@ -1,6 +1,6 @@
 from flask import request
 from flask.views import MethodView
-# import json
+
 
 class NodeRegisterResource(MethodView):
     def post(self):
@@ -9,14 +9,15 @@ class NodeRegisterResource(MethodView):
         node_type = request.form['node_type']
         node_key = request.form['node_key']
         fields = [node_name, node_id, node_type, node_key]
-        
+
         for field in fields:
             if not field:
                 return "Invalid Data", 404
-        
+
         nodes.append({node_name, node_id, node_type, node_key})
         return nodes, 200
-        
+
+
 class NodeListResource(MethodView):
     def get(self):
         return nodes, 200
