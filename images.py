28f2aafe601a84d02f54f040332771dc47f453bc
Eric Harmeling <eric.harmeling@outlook.com>
2018-10-03 08:57:30 -0700

    image processing works (runs)

--- images.py
+++ images.py
@@ -1,14 +1,15 @@
 import subprocess
 
 
-def image_match(file, target):
+def image_match(directory, file, target):
     """
     Checks if image is unique
+    :param directory: Directory containing classify_image.py
     :param file: Input image file
     :param target: Target image category
     :return: True if image from file matches image category, False if not.
     """
-    label = str(subprocess.run(["classify_image.py ", "--image-file " + file]))
+    label = str(subprocess.call(["python", directory + "classify_image.py ", "--image_file", file]))
     if target in label:
         return True
 
