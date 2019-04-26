e0780b4873290770a0da8f772d79597e31a8f5ed
Eric Harmeling <eric.harmeling@outlook.com>
2019-04-25 21:22:32 -0700

    Scanning and checking added to server

--- server.py
+++ server.py
@@ -6,11 +6,16 @@ from resources.mine import *
 # from resources.chain import *
 from resources.users import *
 import json
+import requests
+from requests import HTTPError
 
 # Pull in configuration file
 with open("config.json") as f:
     config_json = json.load(f)
 
+#################
+# SET VARIABLES #
+#################
 # Assign variable to Flask WSGI instance
 app = Flask(__name__)
 
@@ -20,24 +25,65 @@ node_id = config_json["_nodeid"]
 node_type = config_json["_nodetype"]
 node_key = config_json["_key"]
 
+# Define network URL
+URL = config_json["_url"]
+
+# Initialize peer node set
+nodes = dict()
+
+
+# Scan network for other nodes and set port
+def scan(u, p):
+    try:
+        response = requests.get(url=u + p)
+    except HTTPError as http_error:
+        print(f'HTTP error: {http_error}\n Using {p} as node port\n')
+        return p
+    except Exception as error:
+        print(f'Error: {error}\n')
+        return p
+    else:
+        nodes.add(response.json())
+        p += 1
+        scan(u, p+1)
+
+
+node_port = scan(URL, 5000)
+
+# Set variables for other peers
+node_pack = {
+    "name": node_name,
+    "id": node_id,
+    "type": node_type,
+    "port": node_port,
+    "peers": nodes
+}
+
+# Set debug mode
+DEBUG = config_json["_debug"]
+
 # Define local image matcher config variables
 image_dir = config_json["_imagedir"]
 
+######################
+# BLOCKCHAIN PROCESS #
+######################
 # Initialize blockchain
 blockchain = Chain(image_dir)
 
-# initialize node list, you will add to this list with the consensus algorithm
+# Initialize node list, you will add to this list with the consensus algorithm
 blockchain.nodes.append({'node_name': node_name, 'node_id': node_id, 'node_type': node_type,
                          'node_key': node_key})
 
-# Initialize users
-users = list()
-
+##############
+# WEB ROUTES #
+##############
+@app.route('/')
+def check():
+    return json.dumps(node_pack), 200
 
 # Add routes to pages (located in templates)
-
-
-@app.route('/')
+@app.route('/home')
 def index():
     return render_template('home.html')
 
@@ -77,10 +123,10 @@ def contact():
     return render_template('contact.html')
 
 
-# Add routes to resources
-
+#############
+# RESOURCES #
+#############
 # Users
-
 app.add_url_rule('/users/register', view_func=UserRegisterResource.as_view('user_register_resource'), methods=['POST'])
 app.add_url_rule('/users/login', view_func=UserLoginResource.as_view('user_login_resource'), methods=['POST'])
 
@@ -104,5 +150,6 @@ app.add_url_rule('/mine', view_func=MineResource.as_view('mine_resource'), metho
 
 # app.add_url_rule('/chain', view_func=ChainResource.as_view('chain_resource'), methods=['GET'])
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host=URL, port=node_port, debug=DEBUG)
