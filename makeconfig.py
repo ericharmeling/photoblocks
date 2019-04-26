c564c7b21e510e46d353ebaec7325726d520e839
Eric Harmeling <eric.harmeling@outlook.com>
2019-04-25 19:40:54 -0700

    updates to makeconfig script

--- makeconfig.py
+++ makeconfig.py
@@ -6,71 +6,62 @@ import json
 
 
 class Config:
-    def __init__(self):
-        self._key = str(uuid4()).replace('-', '')
-        self._nodeid = str(uuid4()).replace('-', '')
-        self._debug = True
-        self._host = "localhost"
-        self._nodename = "mynode"
-        self._nodetype = "full"
+    def __init__(self, params):
+        self._nodename = params["name"]
+        self._nodetype = params["ntype"]
+        self._imagedir = params["imagedir"]
+        self._key = params["key"]
+        self._nodeid = params["nid"]
+        self._debug = params["debug"]
+        self._url = params["url"]
 
     def as_json(self):
         return json.dumps(self.__dict__)
 
-    @property
-    def key(self):
-        return self._key
 
-    @key.setter
-    def key(self):
-        self._key = str(uuid4()).replace('-', '')
-
-    @property
-    def nodeid(self):
-        return self._nodeid
-
-    @nodeid.setter
-    def nodeid(self, value):
-        self._nodeid = value
-
-
-    @property
-    def debug(self):
-        return self._debug
-
-    @debug.setter
-    def debug(self, value):
-        if type(value) != bool:
-            print("Debug mode must be a boolean value")
-        else:
-            self._debug = value
-
-    @property
-    def host(self):
-        return self._host
-
-    @host.setter
-    def host(self, value):
-        self._host = value
-
-    @property
-    def nodename(self):
-        return self._nodename
-
-    @nodename.setter
-    def nodename(self, value):
-        self._nodename = value
-
-    @property
-    def nodetype(self):
-        return self._nodetype
-
-    @nodetype.setter
-    def nodetype(self, value):
-        self._nodetype = value
-
+# default values
+cwd = "."
+dkey = str(uuid4()).replace('-', '')
+did = str(uuid4()).replace('-', '')
+durl = "localhost"
+dde = True
+dvals = {
+    "imagedir": cwd,
+    "key": dkey,
+    "nid": did,
+    "url": durl,
+    "debug": dde
+}
 
 if __name__ == "__main__":
-    config = Config()
+
+    n = input("What name do you want to give to your node? (Required)\n")
+    t = input("What type of node do you have? (Required, \"full\"|\"light\")\n")
+    indir = input("What directory is your Tensor Flow \"models\" folder in? (Optional, default is CWD)\n")
+    inkey = input("Enter your node's unique public key, for mining rewards.\n"
+              "(If you do not enter a key, one will be created for you.)\n")
+    inid = input("Enter your node unique ID.\n"
+              "(If you do not enter an ID, one will be created for you.)\n")
+    inurl = input("Enter the URL for your local network. (Optional, default is \"localhost\")\n")
+    inde = input("Run in debug mode? (Optional, default is True.)\n")
+    inde = inde == "True"
+    invals = {
+        "name": n,
+        "ntype": t,
+        "imagedir": indir,
+        "key": inkey,
+        "nid": inid,
+        "url": inurl,
+        "debug": inde
+    }
+
+    vals = []
+
+    for key, value in invals.items():
+        if value is "":
+            invals[key] = dvals[key]
+
+    config = Config(invals)
+
     with open("./config.json", mode="w") as f:
         print(config.as_json(), file=f)
