cb2f8016e06e365bf755c0277fef62c7a4906ac9
Eric Harmeling <eric.harmeling@outlook.com>
2021-12-03 13:45:25 -0500

    Moved database connection to sockets, added connection health check

--- photoblocks/main.py
+++ photoblocks/main.py
@@ -24,10 +24,18 @@ def main():
     with open("./photoblocks/network.json") as f:
         network = json.load(f)
 
-    # Connect to database
-    logging.info(f'\nStarting connection to the local database server...')
+    # Test local db connection
     db = redis.Redis(host='redis', port=6379)
-    logging.info(f'\nConnected to the local database server.')
+    while True:
+        try:
+            if db.execute_command('PING'):
+                logging.info(f'\nConnected to the local database server.')
+                break
+            else:
+                continue
+        except Exception as e:
+            logging.error(f'\n{e}')
+            continue
 
     # Construct Node object
     logging.info('\nCreating Node data structure from configuration...')
@@ -36,15 +44,16 @@ def main():
 
     # Start a node socket client
     logging.info('\nStarting node socket client on background thread...')
-    thread = threading.Thread(target=ClientSock, args=(network, node, db))
+    thread = threading.Thread(target=ClientSock, args=(network, node))
     logging.info('\nThread process created.')
     thread.daemon = True
     thread.start()
     logging.info('\nThread process started.')
+    time.sleep(5)
 
     # Start node socket server
     logging.info('\nStarting node socket server on background thread...')
-    thread = threading.Thread(target=serversock, args=(db))
+    thread = threading.Thread(target=serversock, args=())
     logging.info('\nThread process created.')
     thread.daemon = True
     thread.start()
--- photoblocks/networking/serversock.py
+++ photoblocks/networking/serversock.py
@@ -1,17 +1,24 @@
 import socket
 import logging
 import time
+import redis
 
 
-def serversock(db):
+def serversock():
     """
     Broadcasts local node, peer, and blockchain data on network.
     """
-    pack = db.get("pack")
+    db = redis.Redis(host='redis', port=6379)
+    if db.execute_command('PING'):
+        pack = eval(db.get('pack').decode("utf-8"))
+    else:
+        logging.error(
+            f'\nUnable to connect to local Redis server. Please restart the node.')
+        return
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         logging.info('\nNode server socket open for connections.')
         try:
-            sock.bind((pack.host, pack.port))
+            sock.bind(("", pack["port"]))
             sock.listen(5)
             while True:
                 try:
@@ -19,13 +26,13 @@ def serversock(db):
                     data = peer.recv(1024).decode()
                     if data:
                         if data == 'chain':
-                            chain = db.get("chain")
+                            chain = db.get("chain").decode("utf-8")
                             peer.send(chain.encode())
                         elif data == 'pack':
-                            pack = db.get("pack")
+                            pack = db.get("pack").decode("utf-8")
                             peer.send(pack.encode())
                         elif data == 'peerlist':
-                            peerlist = db.get("peers")
+                            peerlist = db.get("peers").decode("utf-8")
                             peer.send(peerlist.encode())
                         elif data == 'ping':
                             peer.send('hello'.encode())
@@ -35,9 +42,11 @@ def serversock(db):
                 except Exception as e:
                     logging.info('\n{e}.')
                     peer.close()
-                    return
-        except socket.error as e:
-            if e.errno == 98:
+                    continue
+        except Exception as e:
+            if e == socket.error and e.errno in (98,99):
                 logging.info(
-                    f'\n{pack.port} is already in use. You should reset the node port')
+                    f'\n{pack["port"]} is already in use. You should reset the node port.')
+            else:
+                logging.info(f'\n{e}.')
             return
